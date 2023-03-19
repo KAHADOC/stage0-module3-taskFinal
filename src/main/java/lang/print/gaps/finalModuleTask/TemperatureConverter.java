734d29fe7489ed9fc1b8c3ee6d20a72979403eb3
Eugene KAHADOC <eugene.bespalov@gmail.com>
2023-03-19 21:52:55 +0300

    all done

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double tempFahr = temperatureCelsius * 9.0/5.0 + 32;
+        System.out.println(tempFahr);
     }
 }
